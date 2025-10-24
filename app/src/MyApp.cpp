b55feb3a9a02ee2959920270a91d8e6160f00638
adjordje <mi19255@alas.matf.bg.ac.rs>
2025-10-24 21:04:58 +0200

    Register MainController in MyApp

--- app/src/MyApp.cpp
+++ app/src/MyApp.cpp
@@ -1,7 +1,9 @@
-#include "MyApp.h"
+#include "MyApp.hpp"
 
 #include "spdlog/spdlog.h"
+#include "MainController.hpp"
 
 void MyApp::app_setup() {
     spdlog::info("App setup completed!");
+    auto main_controller = register_controller<MainController>();
 }
