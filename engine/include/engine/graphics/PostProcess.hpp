97fceccc90eb78251d8c970ee8c95194df3c3236
adjordje <mi19255@alas.matf.bg.ac.rs>
2025-11-01 01:12:18 +0100

    Implement PostProcess class

--- engine/include/engine/graphics/PostProcess.hpp
+++ engine/include/engine/graphics/PostProcess.hpp
@@ -1,8 +1,40 @@
 #ifndef POST_PROCESS_HPP
 #define POST_PROCESS_HPP
+#include <engine/resources/Shader.hpp>
 
 namespace engine::graphics {
-class PostProcess {};
+
+class GraphicsController;
+
+class PostProcess {
+    friend class GraphicsController;
+
+public:
+    void draw();
+
+    void begin_draw();
+
+    void resize(int width, int height);
+
+    void set_enabled(bool enabled);
+
+private:
+    PostProcess() = default;
+
+    PostProcess(int width, int height, const resources::Shader *shader);
+
+    unsigned int m_fbo;
+    unsigned int m_rbo;
+    unsigned int m_texture_color_buffer;
+    unsigned int m_quad_vao = 0;
+    unsigned int m_quad_vbo = 0;
+    const resources::Shader *m_post_process_shader = nullptr;
+    int m_width;
+    int m_height;
+
+    bool m_enabled;
+};
+
 }
 
 #endif //POSTPROCESS_HPP
--- engine/src/PostProcess.cpp
+++ engine/src/PostProcess.cpp
@@ -1,3 +1,98 @@
 #include <engine/graphics/PostProcess.hpp>
+#include <glad/glad.h>
+#include <spdlog/spdlog.h>
 
 using namespace engine::graphics;
+
+PostProcess::PostProcess(int width, int height, const resources::Shader *shader)
+: m_post_process_shader(shader)
+, m_height(height)
+, m_width(width)
+, m_enabled(true) {
+    m_post_process_shader->use();
+    m_post_process_shader->set_int("screen_texture", 0);
+
+    float quadVertices[] = {
+            -1.0f, 1.0f, 0.0f, 1.0f,
+            -1.0f, -1.0f, 0.0f, 0.0f,
+            1.0f, -1.0f, 1.0f, 0.0f,
+
+            -1.0f, 1.0f, 0.0f, 1.0f,
+            1.0f, -1.0f, 1.0f, 0.0f,
+            1.0f, 1.0f, 1.0f, 1.0f
+    };
+
+    glGenFramebuffers(1, &m_fbo);
+    glBindFramebuffer(GL_FRAMEBUFFER, m_fbo);
+
+    glGenTextures(1, &m_texture_color_buffer);
+    glBindTexture(GL_TEXTURE_2D, m_texture_color_buffer);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, m_width,
+                 m_height, 0, GL_RGB, GL_UNSIGNED_BYTE, nullptr);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_texture_color_buffer, 0);
+
+    glGenRenderbuffers(1, &m_rbo);
+    glBindRenderbuffer(GL_RENDERBUFFER, m_rbo);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, m_width, m_height);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, m_rbo);
+
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) == GL_FRAMEBUFFER_COMPLETE) { spdlog::info("Framebuffer is complete!"); }
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+    // quad
+    glGenVertexArrays(1, &m_quad_vao);
+    glGenBuffers(1, &m_quad_vbo);
+    glBindVertexArray(m_quad_vao);
+    glBindBuffer(GL_ARRAY_BUFFER, m_quad_vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void *) 0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void *) (2 * sizeof(float)));
+}
+
+void PostProcess::begin_draw() {
+    if (m_enabled) {
+        glBindFramebuffer(GL_FRAMEBUFFER, m_fbo);
+        resize(m_width, m_height);
+    } else { glBindFramebuffer(GL_FRAMEBUFFER, 0); }
+
+    glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+    glEnable(GL_DEPTH_TEST);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+}
+
+void PostProcess::draw() {
+    if (m_enabled) {
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        m_post_process_shader->use();
+        glBindVertexArray(m_quad_vao);
+        glDisable(GL_DEPTH_TEST);
+        glBindTexture(GL_TEXTURE_2D, m_texture_color_buffer);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+    }
+}
+
+void PostProcess::resize(int width, int height) {
+    m_width = width;
+    m_height = height;
+
+    glBindTexture(GL_TEXTURE_2D, m_texture_color_buffer);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, m_width,
+                 m_height, 0, GL_RGB, GL_UNSIGNED_BYTE, nullptr);
+
+    glBindRenderbuffer(GL_RENDERBUFFER, m_rbo);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, m_width, m_height);
+}
+
+void PostProcess::set_enabled(bool enabled) { m_enabled = enabled; }
+
+
+
+
